3b32a26eb59a6fe442e3a646347e49321ab9a641
Megan Nowak <MNowak16@my.wctc.edu>
2019-10-23 19:36:29 -0500

    Implemented main menu

--- Program.cs
+++ Program.cs
@@ -13,27 +13,48 @@ namespace BlogsConsole
             logger.Info("Program started");
             try
             {
+                var choice = "";
 
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.Blogs.Add(blog);
-                //save changes is the commit to the database
-                db.SaveChanges();
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                do
                 {
-                    Console.WriteLine(item.Name);
+                    Console.WriteLine("1) Display all blogs");
+                    Console.WriteLine("2) Add a blog");
+                    Console.WriteLine("3) Create a post");
+
+                    choice = Console.ReadLine();
+
+                    if (choice == "1")
+                    {
+                        // Display all Blogs from the database
+                        var db = new BloggingContext();
+                        var query = db.Blogs.OrderBy(b => b.Name);
+
+                        Console.WriteLine("All blogs in the database:");
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                    }
+                    if (choice == "2")
+                    {
+                        // Create and save a new Blog
+                        Console.Write("Enter a name for a new Blog: ");
+                        var name = Console.ReadLine();
+
+                        var blog = new Blog { Name = name };
+
+                        var db = new BloggingContext();
+                        db.Blogs.Add(blog);
+                        //save changes is the commit to the database
+                        db.SaveChanges();
+                        logger.Info("Blog added - {name}", name);
+                    }
+                    if (choice == "3")
+                    {
+
+                    }
                 }
+                while (choice == "1" || choice == "2" || choice == "3");
             }
             catch (Exception ex)
             {
